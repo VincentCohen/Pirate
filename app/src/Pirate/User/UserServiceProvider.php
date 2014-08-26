7c1972831c837b9038b468f53b79a2c5a2e4c9f7
Vincent Cohen <V.cohen90@gmail.com>
2014-08-26 09:48:52 +0200

    Update UserServiceProvider.php

--- app/src/Pirate/User/UserServiceProvider.php
+++ app/src/Pirate/User/UserServiceProvider.php
@@ -4,7 +4,7 @@ use Illuminate\Support\ServiceProvider;
 use Entity\EloquentUserRepository;
 use Entity\User;
 
-class PostServiceProvider extends ServiceProvider {
+class UserServiceProvider extends ServiceProvider {
 
     public function register()
     {
